7a68c74271ab7537ec94ad1833d76eca64170187
Jero Sutlovic <jsutlovic@gmail.com>
2013-04-21 11:37:57 -0400

    Allow specifying PORT when running dev mode

--- v2/app.py
+++ v2/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
+import os
 from web.app import app
 
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0', use_reloader=True)
+  app.run(debug=True, host='0.0.0.0', use_reloader=True, port=int(os.environ.get('PORT', 5000)))
